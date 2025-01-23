031eaad50d5cbdaf4b81741118d688b2ca79d1dc
atamaniuksofiia <165192279+atamaniuksofiia@users.noreply.github.com>
2025-01-23 17:34:59 +0200

    adding new animation

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,10 +1,71 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const swiper = new Swiper('.swiper', {
+//     slidesPerView: 'auto',
+//     spaceBetween: 24,
+//     loop: true,
+//     centeredSlides: true,
+//     loopedSlides: 5,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     on: {
+//       slideChangeTransitionEnd: () => {
+//         updateSlideStyles();
+//       },
+//       click: (swiper, event) => {
+//         updateSlideStyles(swiper.clickedIndex);
+//       },
+//     },
+
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 1.2,
+//         spaceBetween: 16,
+//       },
+//       1200: {
+//         slidesPerView: 'auto',
+//         spaceBetween: 24,
+//       },
+//     },
+//   });
+
+//   updateSlideStyles();
+
+//   function updateSlideStyles(clickedIndex = null) {
+//     const slides = document.querySelectorAll('.swiper-slide');
+
+//     // Індекс центрального слайду
+//     const activeIndex = clickedIndex !== null ? clickedIndex : swiper.realIndex;
+
+//     slides.forEach((slide, index) => {
+//       // Обчислюємо різницю між поточним індексом та активним для визначення, чи це центральне зображення
+//       const diff = Math.abs(index - activeIndex);
+
+//       if (diff === 0) {
+//         // Центральне зображення
+//         slide.style.transform = 'scale(1)';
+//         slide.style.filter = 'brightness(1)';
+//         slide.style.zIndex = '2';
+//       } else {
+//         // Бокові зображення (ліва та права)
+//         slide.style.transform = 'scale(0.8)';
+//         slide.style.filter = 'brightness(0.5)';
+//         slide.style.zIndex = '1';
+//       }
+//     });
+//   }
+// });
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper', {
     slidesPerView: 'auto',
     spaceBetween: 24,
     loop: true,
     centeredSlides: true,
-    initialSlide: 2,
     loopedSlides: 5,
     pagination: {
       el: '.swiper-pagination',
@@ -13,19 +74,46 @@ document.addEventListener('DOMContentLoaded', function () {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+      // onClick: (swiper, event) => {
+      //   updateSlideStyles(swiper.activeIndex);
+      // },
     },
-    simulateTouch: true,
-    allowTouchMove: true,
+    on: {
+      slideChangeTransitionEnd: () => {
+        updateSlideStyles();
+      },
+    },
     breakpoints: {
       320: {
         slidesPerView: 1.2,
         spaceBetween: 16,
       },
-
       1200: {
         slidesPerView: 'auto',
         spaceBetween: 24,
       },
     },
   });
+
+  function updateSlideStyles(clickedIndex = null) {
+    const slides = document.querySelectorAll('.swiper-slide');
+
+    const activeIndex = clickedIndex !== null ? clickedIndex : swiper.realIndex;
+
+    slides.forEach((slide, index) => {
+      const diff = Math.abs(index - activeIndex);
+
+      if (diff === 0) {
+        slide.style.transform = 'scale(1)';
+        slide.style.filter = 'brightness(1)';
+        slide.style.zIndex = '2';
+      } else {
+        slide.style.transform = 'scale(0.8)';
+        slide.style.filter = 'brightness(0.5)';
+        slide.style.zIndex = '1';
+      }
+    });
+  }
+
+  updateSlideStyles(swiper.realIndex);
 });
