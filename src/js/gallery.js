ebe1cb1c315b20f7addba5831ee3188e18304765
atamaniuksofiia <165192279+atamaniuksofiia@users.noreply.github.com>
2025-01-23 12:38:41 +0200

    adding new styles

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
       el: '.swiper-pagination',
       clickable: true,
     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     simulateTouch: true,
     allowTouchMove: true,
     breakpoints: {
@@ -25,34 +29,3 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 });
-// document.addEventListener('DOMContentLoaded', function () {
-//   const swiper = new Swiper('.swiper', {
-//     slidesPerView: 'auto',
-//     spaceBetween: 24,
-//     loop: true,
-//     centeredSlides: true,
-//     loopedSlides: 3,
-//     preloadImages: true,
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     simulateTouch: true,
-//     allowTouchMove: true,
-//     watchSlidesProgress: true,
-
-//     breakpoints: {
-//       320: {
-//         slidesPerView: 1.2,
-//         spaceBetween: 16,
-//         centeredSlides: true,
-//       },
-
-//       1200: {
-//         slidesPerView: 1.2,
-//         spaceBetween: 24,
-//         centeredSlides: true,
-//       },
-//     },
-//   });
-// });
